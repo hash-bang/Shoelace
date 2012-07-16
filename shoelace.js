65b46bea3c2ebd434d14fd50e87b0417c473ae28
Matt Carter <m@ttcarter.com>
2012-07-16 13:28:57 +1000

    Added DATA-TOOLTIP support

--- shoelace.js
+++ shoelace.js
@@ -2,3 +2,15 @@
 * Shoelace Javascript file
 * @author Matt Carter <m@ttcarter.com>
 */
+$(function() {
+	/**
+	* DATA-TOOLTIP
+	* Apply a tooltip to an item without having to call $(selector).tooltip()
+	*
+	*	<a href="#" data-tooltip="Tooltip to display"></a>
+	*
+	*/
+	$('[data-tooltip]').each(function() {
+		$(this).tooltip({title: $(this).data('tooltip')});
+	});
+});
