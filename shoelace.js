106404dbfb9196dda3ca64d8ea1075e741224b44
Matt Carter <m@ttcarter.com>
2012-07-16 14:02:30 +1000

    BUGFIX: Strange problem where $(selector).tooltip returns a string. Reverted to using data-tip instead

--- shoelace.js
+++ shoelace.js
@@ -4,13 +4,21 @@
 */
 $(function() {
 	/**
-	* DATA-TOOLTIP
+	* DATA-TIP
 	* Apply a tooltip to an item without having to call $(selector).tooltip()
+	* For various weird reasons 'data-tooltip' cant be used so 'data-tip' will have to suffice.
 	*
 	*	<a href="#" data-tooltip="Tooltip to display"></a>
 	*
+	* Additional parameters:
+	*
+	*	* data-tooltip-placement - Set the tooltip position
+	*
 	*/
-	$('[data-tooltip]').each(function() {
-		$(this).tooltip({title: $(this).data('tooltip')});
+	$('[data-tip]').each(function() {
+		settings = {title: $(this).data('tip')};
+		if ($(this).data('tip-placement'))
+			settings['placement'] = $(this).data('tip-placement');
+		$(this).tooltip(settings);
 	});
 });
