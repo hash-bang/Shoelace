ea53812a151253b0877284a6d754feff0a1cfc89
Matt Carter <m@ttcarter.com>
2012-07-25 15:17:38 +1000

    REFACTOR: Moved code examples into main README.md file

--- shoelace.js
+++ shoelace.js
@@ -3,22 +3,14 @@
 * @author Matt Carter <m@ttcarter.com>
 */
 $(function() {
-	/**
-	* DATA-TIP
-	* Apply a tooltip to an item without having to call $(selector).tooltip()
-	* For various weird reasons 'data-tooltip' cant be used so 'data-tip' will have to suffice.
-	*
-	*	<a href="#" data-tooltip="Tooltip to display"></a>
-	*
-	* Additional parameters:
-	*
-	*	* data-tooltip-placement - Set the tooltip position
-	*
-	*/
-	$('[data-tip]').each(function() {
-		settings = {title: $(this).data('tip')};
-		if ($(this).data('tip-placement'))
-			settings['placement'] = $(this).data('tip-placement');
-		$(this).tooltip(settings);
-	});
+
+/* data-tip {{{ */
+$('[data-tip]').each(function() {
+	settings = {title: $(this).data('tip')};
+	if ($(this).data('tip-placement'))
+		settings['placement'] = $(this).data('tip-placement');
+	$(this).tooltip(settings);
+});
+/* }}} */
+
 });
